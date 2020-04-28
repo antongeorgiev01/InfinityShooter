778596a9f467f99b6308b7010b0d55a0b43a73f1
Anton Georgiev <anton.georgiev@abv.bg>
2020-04-28 16:14:08 +0300

    Commit of the full project in final stage.

---
+++ Assets/Scripts/EnemyGun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+
+    public GameObject EnemyBulletGO;
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("FireEnemyBullet", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FireEnemyBullet()
+    {
+        GameObject playersShip = GameObject.Find("PlayerGO");
+
+        if (playersShip!=null)
+        {
+            GameObject bullet = (GameObject)Instantiate(EnemyBulletGO);
+
+            bullet.transform.position = transform.position;
+
+            Vector2 direction = playersShip.transform.position - bullet.transform.position;
+
+            bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+
+        }
+    }
+}
